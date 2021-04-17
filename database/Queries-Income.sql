81a21e04cc5d73852d50d3037feac5a472a77114
athiranair2000 <athinair2000@gmail.com>
2021-04-17 23:11:26 +0530

    add income queries

---
+++ database/Queries-Income.sql
@@ -0,0 +1,6 @@
+insert into Income values (1,'Scholarship','2,00,000','2021-04-15'),
+							(2,'Internship','65,000','2021-04-14'),
+							(3,'allowance','35,000','2021-04-17'),
+							(4,'Parttime Job','10,000','2021-04-16');
+							
+								
