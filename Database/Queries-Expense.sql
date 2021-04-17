7ea2c995e784edeb2142392654873af05be32a25
Sumeghana Reddy <sumeghanareddy2001@gmail.com>
2021-04-17 22:53:46 +0530

    ADD DataBase

---
+++ Database/Queries-Expense.sql
@@ -0,0 +1,6 @@
+insert into Expense values (1,'Collage_fee','2,40,000','2021-04-15'),
+							(2,'Hostel_fee','65,000','2021-04-14'),
+							(3,'Mess_fee','35,000','2021-04-17'),
+							(4,'Travel_expenses','10,000','2021-04-16'),
+							(5,'Accessories','7,000','2021-04-16');
+								
---
+++ Database/Queries-Expense_Tracker.sql
@@ -0,0 +1,2 @@
+insert into Expense_Tracker values ('1','370000','352000','12000');
+							
