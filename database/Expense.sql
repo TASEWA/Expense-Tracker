4996a5a5a216624e12990f2edbc9f8132f32b4a8
Sumeghana Reddy <53039727+sumeghana@users.noreply.github.com>
2021-04-20 19:15:57 +0530

    Update Expense.sql

--- database/Expense.sql
+++ database/Expense.sql
@@ -1,18 +1,24 @@
-CREATE TABLE Expense( Expense_id serial  primary key,
-				   Expense_item char(20) not null,
+CREATE TABLE Expense( 			
+					 id int ,
+					 Expense_id serial  primary key,
+				    	 Expense_item char(20) not null,
 					 Expense_amount char(10)  not null,
-					 Expense_Date DATE not null
+					 Expense_Date DATE not null,
+					foreign key (id) references UserProfile(id)
 				  );
 
-CREATE TABLE Income( Income_id serial  primary key,
-				     Income_item char(20) not null,
+CREATE TABLE Income( 			 
+					 id int ,
+					 Income_id serial  primary key,
+				    	 Income_item char(20) not null,
 					 Income_amount char(10) not null,
-					 Income_Date DATE not null
+					 Income_Date DATE not null,
+					foreign key (id) references UserProfile(id)
 				  );
 CREATE TABLE Expense_Tracker (   
-							  id int ,
+					      id int ,
 				              Total_income int ,
-					          Total_expense int ,
-					          Total_balance int ,
+					      Total_expense int ,
+					      Total_balance int ,
 	                          foreign key (id) references UserProfile(id)
 				             );
