294bd3134f42b070d125a291c38cc3bd2f9ff024
Lohitha02 <lohithabhagam123@gmail.com>
2021-04-20 22:33:00 +0530

    Update: index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const cors=require('cors');
+const cors = require('cors');
 const PORT = 8000;
-const pool=require('./db.js');
+const pool = require('./db');
 
-//app.use('/static', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, 'public')))
 app.use(cors());
 app.use(express.json());
 //request to accept body
@@ -14,11 +14,18 @@ app.get('/', (req, res) => {
 });
 
 //create a list
-app.post("/list",async(req,res)=>{
+/*app.post("/list",async(req,res)=>{
 try {
-    res.json(req.body);
+    const { Expense_item } = req.body;
+    const newExpense = await pool.query("INSERT INTO Expense (Expense_item) VALUES($1) RETURNING *, 
+    [Expense_item]
+	);
+
+    res.json(newExpense);
+
+    console.log(req.body);
 } catch (error) {
     console.error(err.message);
 }
-})
+})*/
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
