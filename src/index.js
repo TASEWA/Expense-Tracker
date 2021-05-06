b8bdd2d16e46a894ed209446fb3e19fe1b699b34
sahanaranga <sahanarangasv@gmail.com>
2021-05-06 23:12:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,60 +8,47 @@ const pool = require('./db');
 app.use('/', express.static(path.join(__dirname, 'public')))
 app.use(cors());
 app.use(express.json());
+
 //request to accept body
 app.get('/', (req, res) => {
     res.send('Crashed!');
 });
 
-app.get('/list',async(req,res)=>{
-  try {
-  
-  const expdetails=await pool.query("SELECT * FROM Expense");
-  res.json(expdetails);
-  } catch (error) {
-  console.error(err.message);
-  }
-  });
-//get
+app.get('/list', function(req, res, next) {
+	pool.connect();
 
-app.post('/list', async(req, res) => {
-    try {
-        const { Expense_item } = req.body;
-        const { Expense_amount } = req.body;
-        const { Expense_date } = req.body;
-        const newExpense = pool.query("INSERT INTO Expense (Expense_item,Expense_amount,Expense_date) VALUES($1) RETURNING *", [Expense_item], [Expense_amount], [Expense_date]);
-        res.json('Inserted!');
-        console.log(req.body);
-    } catch (err) { console.log(err.message); }
+	pool.query('SELECT * FROM expense', function(error, results, fields){
+		res.send(results);
+	});
+});
+
+app.get('/get_expense_tracker_details', function(req, res, next) {
+	pool.connect();
+
+	pool.query('SELECT * FROM expense_tracker', function(error, results, fields){
+		res.send(results);
+	});
 
 });
-//post
 
+app.post('/post_user_details', async function(req, res, next) {
 
-app.delete('/list', async(req, res) => {​​​​​
-    try {​​​​​
-        const {​​​​​ expense_id }​​​​​ = req.params;
-        const deleteexpense = await pool.query("DELETE FROM Expense WHERE expense_id = $1", [expense_id]);
-        res.json("Deleted!");
-    }​​​​​ catch (err) {​​​​​
-        console.log(err.message);
-    }​​​​​
-}​​​​​);
-​
+	pool.connect();
 
-app.put('/list', async (req, res) => {
-    try {
-      const { id } = req.params;
-      const { Expense_amount } = req.body;
-      const updateexpense = await pool.query(
-        "UPDATE list SET Expense_amount = $1 WHERE Expense_id = $2",
-        [Expense_amount, id]
-      );
-      res.json("updated!");
-    } catch (err) {
-      console.error(err.message);
-    }
-  });
-//update
+	const { name } = req.body;
+        const { username } = req.body;
+        const { password } = req.body;
+	const { email } = req.body;
+
+	let sql = 'UPDATE userProfile set name = ?, username = ?, password = ?, email = ? where id = 3';
+
+	await pool.query(sql, [name, username, password, email], function(error, results, fields)
+	{
+  		if (error)
+    			return console.error(error.message);
+  		
+  		console.log('Rows affected:', results.affectedRows);
+	});
+});
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
