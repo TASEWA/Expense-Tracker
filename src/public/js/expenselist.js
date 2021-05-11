d1c6683a1bccacb3de6cb4d8b82071c336cd981e
Athi_Nair_2000 <athinair2000@gmail.com>
2021-05-11 15:01:16 +0530

    Update expense form functionality

--- src/public/js/expenselist.js
+++ src/public/js/expenselist.js
@@ -1,16 +1,129 @@
-$(document).ready(function () {
-    $.ajax(
-        {
-            type: 'GET',
-            url: '/list',
-            datatype: 'JSON',
-            success: function (result) {
-                $('#expItem').append(result[0].item);
-                $('#expValue').append(result[0].amount);
-                console.log(result);
-            }
-        });
-});
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("addBtn");
+const span = document.getElementsByClassName("close")[0];
+var xhttp;
+xhttp = new XMLHttpRequest();
+btn.onclick = function () {
+  expName.value = "";
+  expNumber.value = "";
+  expenseForm.style.display = "block";
+  editForm.style.display = "none";
+  modal.style.display = "block";
+  budgetform.style.display = "none";
+};
+span.onclick = function () {
+  modal.style.display = "none";
+};
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
+const amountInput = document.getElementById("number");
+const addForm = document.getElementById("addForm");
+const budgetAmount = document.getElementById("budgetAmount");
+const balanceAmount = document.getElementById("balanceAmount");
+
+const editForm = document.getElementById("editForm");
+const saveEdit = document.getElementById("saveEdit");
+const editExpValue = document.getElementById("editExpValue");
+const editExpNumber = document.getElementById("editExpNumber");
+
+const expForm = document.getElementById("expForm");
+const expensesAmount = document.getElementById("expensesAmount");
+const expValue = document.getElementById("expValue");
+const displayExpenses = document.getElementById("displayExpenses");
+const expenseForm = document.getElementById("expense-form");
+const budgetform = document.getElementById("budgetform");
+
+let expName = document.getElementById("expName");
+let expNumber = document.getElementById("expNumber");
+let id = 0;
+let details = [];
+
+
+function addExpenses(name, number) {
+  if (!name.length || !number.length) {
+    expName.style.border = "1px solid #b80c09";
+    expName.placeholder = "input can not be empty";
+    expName.style.color = "#b80c09";
+
+    expNumber.style.border = "1px solid #b80c09";
+    expNumber.placeholder = "input can not be empty";
+    expNumber.style.color = "#b80c09";
+
+  } else {
+    id++;
+    xhttp.open("POST", '/post_expense_details', true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify(data));
+    expName.value = "";
+    expNumber.value = "";
+    
+  }
+}
+
+function displayExp(details) {
+  xhttp.open("GET", '/list', true);
+  xhttp.send();
+  expValue.innerHTML = null;
+  for (i = 0; i < details.length; i++) {
+    expValue.innerHTML += `
+    <div class="expValue" id="${details[i].id}">
+      <div id="expTitleName" class="exp"><p>${details[i].$('#expName').append(result[0].item)}</p></div>
+      <div id="expValueAmount" class="exp"><p> <span>$ </span> ${details[i].$('#expNumber').append(result[0].amount)}</p></div>
+      <div id="edite_delete">
+        <p>
+          <button id="${details[i].id}" onclick="editExpDetails(${details[i].id})"></button> 
+          <button id="${details[i].id}" onclick="delExpenseDetails(${details[i].id})"></button>
+        </p>
+      </div>
+    </div>
+  `;
+  }
+
+  displayExpenses.style.display = "block";
+}
+
+
+function delExpenseDetails(id) {
+  let index = details.findIndex((item) => item.id === id);
+  xhttp.open("DELETE", '/list');
+  xhttp.send();
+  displayExp(details);
+}
+
+function editExpDetails(id) {
+  expenseForm.style.display = "none";
+  budgetform.style.display = "none";
+  editForm.style.display = "block";
+  details.findIndex((item) => {
+    if (item.id === id) {
+      editExpName.value = item;
+      editExpNumber.value = number;
+      modal.style.display = "block";
+    }
+  });
+}
+
+function getExpValue(editExpName, editExpNumber, id) {
+  edited = details.findIndex((obj) => obj.id == id);
+  details[edited].name = editExpName;
+  details[edited].number = parseInt(editExpNumber);
+  displayExp(details);
+}
+
+
+
+saveEdit.addEventListener("submit", (e) => {
+  e.preventDefault();
+  getExpValue(editExpName.value, editExpNumber.value, saveEdit.children[2].id);
+
+expForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addExpenses(expName.value, expNumber.value);
+});
+});
 
 
